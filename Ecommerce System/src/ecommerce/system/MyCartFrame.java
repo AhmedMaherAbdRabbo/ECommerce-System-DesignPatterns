5de64201d9ab0bd6e10411e5d870e58c9c9096f3
Mohamed khalifa <mohammedelkady8889@gmail.com>
2025-12-22 23:23:21 +0200

    Cart Frame

--- Ecommerce System/src/ecommerce/system/MyCartFrame.java
+++ Ecommerce System/src/ecommerce/system/MyCartFrame.java
@@ -4,7 +4,7 @@
  */
 package ecommerce.system;
 
-import frontend.*;
+
 
 /**
  *
@@ -35,6 +35,11 @@ public class MyCartFrame extends javax.swing.JFrame {
         Orders = new javax.swing.JButton();
         AddProduct = new javax.swing.JButton();
         MyCart = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        table_cart = new javax.swing.JTable();
+        bt_checkout = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        label_total = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -109,8 +114,37 @@ public class MyCartFrame extends javax.swing.JFrame {
                 .addComponent(AddProduct, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(44, 44, 44)
                 .addComponent(Orders, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(117, Short.MAX_VALUE))
+                .addContainerGap(122, Short.MAX_VALUE))
         );
+
+        table_cart.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Product", "Quantity", "Price", "Total"
+            }
+        ));
+        table_cart.setColumnSelectionAllowed(true);
+        jScrollPane1.setViewportView(table_cart);
+        table_cart.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+
+        bt_checkout.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        bt_checkout.setText("Checkout");
+        bt_checkout.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bt_checkoutActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel1.setText("Total");
+
+        label_total.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        label_total.setText("$");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -119,7 +153,18 @@ public class MyCartFrame extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(776, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 740, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(28, 28, 28)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(label_total, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(bt_checkout, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(18, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -127,6 +172,19 @@ public class MyCartFrame extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 439, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1)
+                            .addComponent(label_total))
+                        .addGap(37, 37, 37))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(bt_checkout, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(35, 35, 35))))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -162,6 +220,10 @@ public class MyCartFrame extends javax.swing.JFrame {
     private void MyCartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MyCartActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_MyCartActionPerformed
+
+    private void bt_checkoutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_checkoutActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_bt_checkoutActionPerformed
 
     /**
      * @param args the command line arguments
@@ -205,7 +267,12 @@ public class MyCartFrame extends javax.swing.JFrame {
     private javax.swing.JButton MyCart;
     private javax.swing.JButton Orders;
     private javax.swing.JButton Products;
+    private javax.swing.JButton bt_checkout;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel label_total;
+    private javax.swing.JTable table_cart;
     // End of variables declaration//GEN-END:variables
 }
